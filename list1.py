8b811ae18caf9ea5b09baf9d784fb5be863ebaac
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-31 00:12:22 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,35 @@
+a=[]
+print(a)
+print(type(a))
+print(id(a))
+print(len(a))
+
+b=list()
+print(b)
+print(type(b))
+print(id(b))
+print(len(b))
+
+c=[10,20,30,40,50]
+print(c)
+print(type(c))
+print(id(c))
+print(len(c))
+
+d=[10,123.123,3+4j,True,'Rahul Singh']
+print(d)
+print(type(d))
+print(id(d))
+print(len(d))
+
+e=[1,2,3,1,2,1]
+print(e)
+print(type(e))
+print(id(e))
+print(len(e))
+
+f=list('Rahul Singh')
+print(f)
+print(type(f))
+print(id(f))
+print(len(f))
