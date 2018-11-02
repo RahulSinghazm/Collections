7cee328b92f9a60b6a09f79d042be9afb605c2fc
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-11-03 00:09:44 +0530

    Update nestedlist4.py

--- nestedlist4.py
+++ nestedlist4.py
@@ -1,8 +1,8 @@
-a=[[1001,'Rahul',5000.00],[2002,'Rohit',6000.00],[3003,'Alok',7000.00]]
+a=[[1001,'Rahul',8000.00],[3003,'Rohit',6000.00],[2002,'Alok',4000.00]]
 print('Befor Sorting')
 for p in a:
     print(p)
-x.sort()
+a.sort()
 print('After Sorting')
 for q in a:
     print(q)
