4c3c623a0b9e8983b6c8d2f188f0b1a58b6f80df
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-11-02 23:57:44 +0530

    Add files via upload

---
+++ nestedlist4.py
@@ -0,0 +1,8 @@
+a=[[1001,'Rahul',5000.00],[2002,'Rohit',6000.00],[3003,'Alok',7000.00]]
+print('Befor Sorting')
+for p in a:
+    print(p)
+x.sort()
+print('After Sorting')
+for q in a:
+    print(q)
