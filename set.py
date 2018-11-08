d7d842e41d9d2cb90e689d0aab7bf427e026c6c3
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-11-08 23:59:34 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,20 @@
+a={10}
+print(a)
+print(type(a))
+print(len(a))
+print(id(a))
+
+b=set()
+print(b)
+print(type(b))
+print(len(b))
+print(id(b))
+
+c={10,20,30,40,50}
+print(c)
+
+d={10,20,10,20,10,30}
+print(d)
+
+e={100,123.123,True,3+5j,"Rahul Singh"}
+print(e)
