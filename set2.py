95dfcefe6012ce1a07ff514e9102c28b5143f17e
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-11-09 01:26:26 +0530

    Add files via upload

---
+++ set2.py
@@ -0,0 +1,14 @@
+a={10,20,30,40,50}
+print(a)
+print(30 in a)
+print(25 in a)
+for p in a:
+    print(p)
+b={(10,20,30),(40,50,60),(70,80,90)}
+print(a)
+for q in b:
+    print(q,type(q))
+for a,b,c in b:
+    print(a,b,c)
+    
+    
