181e704a8fe8531770d37649cef9976ae394f2ee
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-11-09 13:06:09 +0530

    Add files via upload

---
+++ dict3.py
@@ -0,0 +1,12 @@
+a={"Python":99,"Java":90,"Oracle":82,"Django":99}
+print(a)
+a['Oracle']=92
+print(a)
+a[90]='Java OOPS'
+print(a)
+b={100:'Hyd',123.123:'Vns',True:'Azm',3+6j:'Chennai'}
+print(b)
+c={10:1000,20:12.123,30:True,40:3+6j}
+print(c)
+d={(1,2,3):"Flash","Hyd":[5,6,7]}
+print(d)
